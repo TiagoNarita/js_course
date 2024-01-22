cfe5635f3e22e2ecce4a6cf3d942914f8a67c0b8
Tiago <tiagobnarita@gmail.com>
2024-01-22 20:59:34 -0300

    day-03

---
+++ README.md
@@ -0,0 +1,2 @@
+# 100_Projects_for_web
+# 100_Projects_for_web
---
+++ app.js
@@ -0,0 +1,9 @@
+function show4(a, b, c, d) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+}
+numbers = [1,2,3,4]
+
+show4(... numbers)
