701e561939d0de7b67464ecdc75ed12a9987116d
Tiago <tiagobnarita@gmail.com>
2024-01-25 22:27:18 -0300

    part 2

--- day25_class/indes.js
+++ day25_class/indes.js
@@ -7,3 +7,19 @@ let person = {
 let clone = [{...person, profission: "Programer"}, "oi"]
 
 console.log(clone);
+
+function f(){
+    return[1,2]
+}
+
+let a, b
+[a,b] = f();
+console.log(a);
+console.log(b);
+
+const colors = ['green','red',"blue","yellow","orange"];
+let color1, color2, color3
+[color1, color2, color3] = colors
+console.log(color1);
+
+ 
