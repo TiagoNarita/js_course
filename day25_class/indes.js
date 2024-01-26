e9497ea6415306c4898f61fc821d6567d23b7b02
Tiago <tiagobnarita@gmail.com>
2024-01-25 22:48:17 -0300

    ternary class

--- day25_class/indes.js
+++ day25_class/indes.js
@@ -4,22 +4,37 @@ let person = {
     gender: "Male"
 }
 
-let clone = [{...person, profission: "Programer"}, "oi"]
+let clone = [{ ...person, profission: "Programer" }, "oi"]
 
 console.log(clone);
 
-function f(){
-    return[1,2]
+function f() {
+    return [1, 2]
 }
 
 let a, b
-[a,b] = f();
+[a, b] = f();
 console.log(a);
 console.log(b);
 
-const colors = ['green','red',"blue","yellow","orange"];
+const colors = ['green', 'red', "blue", "yellow", "orange"];
 let color1, color2, color3
 [color1, color2, color3] = colors
 console.log(color1);
 
- 
+//the ternary operator in js
+
+//condition ? expriftrue : exprisfalse;
+
+function password(ps) {
+    if (ps === 8) {
+        return "Strong Password"
+    } else {
+        return"Your password shoudb be 8 characters"
+    }
+}
+
+const res = password(2)
+console.log(res);
+
+//refactoring
