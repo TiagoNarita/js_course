355f2e45486892c624779375e16e7832c3a1a7ba
Tiago <tiagobnarita@gmail.com>
2024-01-28 15:48:49 -0300

    for each

--- day26_class/index.js
+++ day26_class/index.js
@@ -7,3 +7,29 @@ for (let index in list) {
     console.log(index);
     console.log(list);
 }
+
+//for off generly used for arrays
+let text = "hello";
+
+for (const letras of text) {
+    console.log(letras);
+}
+
+list = ["one", "two", "three", "four"]
+
+for (const numbers of list) {
+    console.log(numbers);
+}
+
+//for each
+
+let colors = ["white", "blue", "red", "green"];
+
+colors.forEach(color => console.log(color));
+console.log(colors);
+
+ colors = colors.map((word) => {
+    return word[0].toUpperCase() + word.substring(1);
+});
+
+console.log(colors);
