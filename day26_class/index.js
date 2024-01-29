91331280ba7d265e9cf79a1bf20a75f80b65504f
Tiago <tiagobnarita@gmail.com>
2024-01-28 23:40:31 -0300

    .map()

--- day26_class/index.js
+++ day26_class/index.js
@@ -28,19 +28,29 @@ let colors = ["white", "blue", "red", "green"];
 colors.forEach(color => console.log(color));
 console.log(colors);
 
- colors = colors.map((word) => {
+colors = colors.map((word) => {
     return word[0].toUpperCase() + word.substring(1);
 });
 
 console.log(colors);
 
-let numberus = [1,2,3,4,18];
+let numberus = [1, 2, 3, 4, 18];
 let sum = 0;
 
 const add = (number) => {
     sum += number;
 }
 
-numberus.forEach(add);
+numberus.forEach(add)
 
 console.log(sum);
+
+//the map() method creates a new array populated with the results of calling a 
+//provided function on every elementi in the calling array.
+
+numberus = [1, 2, 3, 4, 5];
+
+let double = numberus.map(num => num * 2);
+
+console.log(double);
+
