4c568c34685fb9bbe06e8ad5d568d1e6f1ce0d67
Tiago <tiagobnarita@gmail.com>
2024-02-02 18:34:15 -0300

    update the game with weapons

--- freeCodeCamp_js/script.js
+++ freeCodeCamp_js/script.js
@@ -21,24 +21,66 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function goTown() {
+    update(locations[0]);
+}
+
 function goStore() {
     update(locations[1]);
 }
 
 function goCave() {
-    console.log("Going to cave.");
+    update(locations[2]);
 }
 
 function fightDragon() {
     console.log("Fighting dragon.");
 }
 
-function buyHealth() {}
-function buyWeapon() {}
-function goTown() {
-    update(locations[0]);
+function buyHealth() {
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health.";
+    }
 }
+function buyWeapon() {
+    if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+}
+function fightSlime() {}
+function fightBeast() {}
 
+const weapons = [
+    {
+        name: "stick",
+        power: 5,
+    },
+    {
+        name: "dagger",
+        power: 30,
+    },
+    {
+        name: "claw hammer",
+        power: 50,
+    },
+    {
+        name: "sword",
+        power: 100,
+    },
+];
 const locations = [
     {
         name: "town square",
@@ -55,6 +97,16 @@ const locations = [
         ],
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store.",
+    },
+    {
+        name: "cave",
+        "button text": [
+            "Fight slime",
+            "Fight fanged beast",
+            "Go to town square",
+        ],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: " You enter the cave. You see some monsters.",
     },
 ];
 
