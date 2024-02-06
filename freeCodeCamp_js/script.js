ec1b1f5d299220ad96f72f0ef43c32a14613fc6e
Tiago <tiagobnarita@gmail.com>
2024-02-05 23:06:40 -0300

    lose function created

--- freeCodeCamp_js/script.js
+++ freeCodeCamp_js/script.js
@@ -135,7 +135,13 @@ function attack() {
     text.innerText +=
         " You attack it with your " + weapons[currentWeapon].name + ".";
     health -= monsters[fighting].level;
-    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.parse * xp);
+    monsterHealth -=
+        weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+        lose();
+    }
 }
 
 function dodge() {}
