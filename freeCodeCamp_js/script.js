17e77048320c27c09ea7a0684a113666cf16c699
Tiago <tiagobnarita@gmail.com>
2024-02-05 23:01:00 -0300

    go fight update

--- freeCodeCamp_js/script.js
+++ freeCodeCamp_js/script.js
@@ -125,6 +125,9 @@ const monsters = [
 function goFight() {
     update(locations[3]);
     monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
 }
 
 function attack() {
